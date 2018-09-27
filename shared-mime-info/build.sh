27babc86604291c1093da9fa550ac6a5c212cc0e
Mark Howison <mhowison@brown.edu>
2018-09-27 09:53:07 -0400

    shared-mime-info 1.8_2

--- shared-mime-info/build.sh
+++ shared-mime-info/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-if [ $(uname) == "Darwin" ]; then
-  export LDFLAGS="$LDFLAGS -Wl,-rpath,$PREFIX/lib"
-  export CFLAGS=$CFLAGS" -I${PREFIX}/include"
-  ./configure --prefix=$PREFIX
-else
-  ./configure --prefix=$PREFIX
-fi
+
+set -e
+
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:/usr/lib64/pkgconfig:/usr/share/pkgconfig
+
+./configure --prefix=$PREFIX
 make
 make install
